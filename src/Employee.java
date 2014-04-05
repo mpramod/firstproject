cae7e68340953b7299f6cda1060298f8b037fada
mpramod <pramodachary96@gmail.com>
2014-04-05 15:14:02 +0530

    2 files been added

--- src/Employee.java
+++ src/Employee.java
@@ -3,6 +3,6 @@ public class Employee {
 
 	private String name;
 	public static void main(String args[]) {
-		System.out.println("Employee object");
+		System.out.println("Ebject");
 	}
 }
--- src/User.java
+++ src/User.java
@@ -3,6 +3,6 @@ public class User {
 
 	private String name;
 	public static void main(String args[]) {
-		System.out.println("User object");
+		System.out.println("User object mmm");
 	}
 }
